4bf5855af3cf954d2a2591ee3134ab1357455d5b
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-03 00:17:50 -0700

    Completed fields for  model for resetcode

--- app/Models/version1/ResetCode.php
+++ app/Models/version1/ResetCode.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ResetCode extends Model
-{
-    use HasFactory;
+{    
+    use Notifiable;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'resetcode_id';
+
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'resetcode_id', 
+        'user_id',
+        'resetcode',
+        'used',
+        'created_at',
+        'updated_at',
+    ];
+
 }
