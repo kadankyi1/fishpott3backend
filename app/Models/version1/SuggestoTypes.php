a6cbe01466b58c5ee5515bd6e4968cc33685ad81
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-11 04:59:36 -0700

    Created model and migration for SuggestoTypes

---
+++ app/Models/version1/SuggestoTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\version1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SuggestoTypes extends Model
+{
+    use HasFactory;
+}
