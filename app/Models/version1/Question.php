a4e2e67ee63c5ad063d024a8fcf171b2ae8b4073
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-11 04:52:30 -0700

    Added features for question model

--- app/Models/version1/Question.php
+++ app/Models/version1/Question.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+        /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'question_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question_id', 
+        'question_sys_id', 
+        'question_question',
+        'question_answer_1',
+        'question_answer_2',
+        'question_answer_3',
+        'question_answer_4',
+        'question_answer_implied_traits_1',
+        'question_answer_implied_traits_2',
+        'question_answer_implied_traits_3',
+        'question_answer_implied_traits_4',
+        'question_flagged',
+        'question_maker_investor_id',
+        'question_maker_investor_name',
+        'created_at',
+        'updated_at',
+    ];
+
 }
--- database/migrations/2021_10_11_114821_create_questions_table.php
+++ database/migrations/2021_10_11_114821_create_questions_table.php
@@ -14,8 +14,25 @@ class CreateQuestionsTable extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('question_id');
+            $table->string('question_sys_id', 255)->unique();
+            $table->string('question_question', 255)->default;
+            $table->string('question_answer_1', 255);
+            $table->string('question_answer_2', 255);
+            $table->string('question_answer_3', 255)->default("");
+            $table->string('question_answer_4', 255)->default("");
+            $table->text('question_answer_implied_traits_1');
+            $table->text('question_answer_implied_traits_2');
+            $table->text('question_answer_implied_traits_3');
+            $table->text('question_answer_implied_traits_4');
+            $table->boolean('question_broadcasted')->default(false);
+            $table->boolean('question_flagged')->default(false);
             $table->timestamps();
+        });
+
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('question_maker_investor_id', 255);
+            $table->foreign('question_maker_investor_id')->references('investor_id')->on('users');
         });
     }
 
