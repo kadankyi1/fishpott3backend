73b902ca599d9388e75ca8db5bc11b1402dfcf70
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-12 04:37:24 -0700

    Added model features to business model

--- app/Models/version1/Business.php
+++ app/Models/version1/Business.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Business extends Model
 {
     use HasFactory;
+        /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'business_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'business_id', 
+        'business_sys_id', 
+        'business_type',
+        'business_full_name',
+        'business_short_name',
+        'business_business_ceo_name',
+        'business_business_pott_name',
+        'business_business_pitch',
+        'business_business_address',
+        'business_business_country',
+        'business_business_revenue_last_year',
+        'business_business_revenue_last_year',
+        'business_flagged',
+        'created_at',
+        'updated_at',
+    ];
+
 }
