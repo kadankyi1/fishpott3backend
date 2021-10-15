951124a140d7f9b27e1c7b7c0e8d718b186a2154
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-15 05:32:24 -0700

    Updated CreateSuggestionTypesTable error

--- database/migrations/2021_10_12_115913_create_suggestion_types_table.php
+++ database/migrations/2021_10_12_115913_create_suggestion_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSuggestionsTypesTable extends Migration
+class CreateSuggestionTypesTable extends Migration
 {
     /**
      * Run the migrations.
