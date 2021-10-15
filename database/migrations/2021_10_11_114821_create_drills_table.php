d4187c699d58f9ac681599aaefed825b2c459f6c
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-15 04:58:14 -0700

    Updated CreateQuestionsTable to CreatedDrillsTable

--- database/migrations/2021_10_11_114821_create_drills_table.php
+++ database/migrations/2021_10_11_114821_create_drills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionsTable extends Migration
+class CreateDrillsTable extends Migration
 {
     /**
      * Run the migrations.
