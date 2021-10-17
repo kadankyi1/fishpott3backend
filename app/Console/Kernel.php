16adff25fe12ea2f9afe79bcf21be346b71af71c
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-16 22:29:06 -0700

    Updated command loading route

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SendDrillReadyNotificationCommand::class,
+        Commands\version1\SendDrillReadyNotificationCommand::class,
     ];
 
     /**
