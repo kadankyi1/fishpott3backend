4e84498388822a5c903d7143a2dd11ee0c1d66b6
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-17 04:13:57 -0700

    Added fields for business db table migration

--- database/migrations/2021_10_12_113515_create_businesses_table.php
+++ database/migrations/2021_10_12_113515_create_businesses_table.php
@@ -14,7 +14,19 @@ class CreateBusinessesTable extends Migration
     public function up()
     {
         Schema::create('businesses', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('drill_id');
+            $table->string('drill_sys_id', 255)->unique();
+            $table->string('drill_question', 255)->default;
+            $table->string('drill_answer_1', 255);
+            $table->string('drill_answer_2', 255);
+            $table->string('drill_answer_3', 255)->default("");
+            $table->string('drill_answer_4', 255)->default("");
+            $table->text('drill_answer_implied_traits_1');
+            $table->text('drill_answer_implied_traits_2');
+            $table->text('drill_answer_implied_traits_3');
+            $table->text('drill_answer_implied_traits_4');
+            $table->boolean('drill_passed_as_suggestion')->default(false);
+            $table->boolean('drill_flagged')->default(false);
             $table->timestamps();
         });
     }
