83aa30895f47a3bec24e9e7316f2048d2bfcbba8
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-17 04:16:42 -0700

    Added foreign key check for drop table in drills migration

--- database/migrations/2021_10_11_114821_create_drills_table.php
+++ database/migrations/2021_10_11_114821_create_drills_table.php
@@ -43,6 +43,8 @@ class CreateDrillsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('drills');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
