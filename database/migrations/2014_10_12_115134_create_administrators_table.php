ea2d8e96aa434c8342dcca6f32407a819433a11b
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-18 05:02:30 -0700

    Columns added to administrator migration

--- database/migrations/2014_10_12_115134_create_administrators_table.php
+++ database/migrations/2014_10_12_115134_create_administrators_table.php
@@ -14,8 +14,23 @@ class CreateAdministratorsTable extends Migration
     public function up()
     {
         Schema::create('administrators', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('admin_id');
+            $table->string('admin_surname', 255);
+            $table->string('admin_firstname', 255);
+            $table->string('admin_othernames', 255)->nullable();
+            $table->string('admin_phone_number', 255)->unique();
+            $table->string('admin_email', 255)->unique();
+            $table->string('admin_pin', 255);
+            $table->string('password', 255);
+            $table->text('admin_scope');
+            $table->boolean('admin_flagged');
+            $table->unsignedBigInteger('creator_admin_id');
             $table->timestamps();
+        });
+
+        Schema::table('drills', function (Blueprint $table) {
+            $table->string('drill_maker_investor_id', 255);
+            $table->foreign('drill_maker_investor_id')->references('investor_id')->on('users');
         });
     }
 
