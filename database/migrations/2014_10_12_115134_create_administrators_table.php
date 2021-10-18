d4d537d21b2908b03046d2cb6b1fe5a5bc3ae73d
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-18 05:04:20 -0700

    Columns added to administrator migration

--- database/migrations/2014_10_12_115134_create_administrators_table.php
+++ database/migrations/2014_10_12_115134_create_administrators_table.php
@@ -14,23 +14,23 @@ class CreateAdministratorsTable extends Migration
     public function up()
     {
         Schema::create('administrators', function (Blueprint $table) {
-            $table->bigIncrements('admin_id');
-            $table->string('admin_surname', 255);
-            $table->string('admin_firstname', 255);
-            $table->string('admin_othernames', 255)->nullable();
-            $table->string('admin_phone_number', 255)->unique();
-            $table->string('admin_email', 255)->unique();
-            $table->string('admin_pin', 255);
+            $table->bigIncrements('administrator_id');
+            $table->string('administrator_surname', 255);
+            $table->string('administrator_firstname', 255);
+            $table->string('administrator_othernames', 255)->nullable();
+            $table->string('administrator_phone_number', 255)->unique();
+            $table->string('administrator_email', 255)->unique();
+            $table->string('administrator_pin', 255);
             $table->string('password', 255);
-            $table->text('admin_scope');
-            $table->boolean('admin_flagged');
-            $table->unsignedBigInteger('creator_admin_id');
+            $table->text('administrator_scope');
+            $table->boolean('administrator_flagged');
+            $table->unsignedBigInteger('creator_administrator_id');
             $table->timestamps();
         });
 
         Schema::table('administrators', function (Blueprint $table) {
-            $table->string('admin_user_pottname', 255);
-            $table->foreign('admin_user_pottname')->references('user_pottname')->on('users');
+            $table->string('administrator_user_pottname', 255);
+            $table->foreign('administrator_user_pottname')->references('user_pottname')->on('users');
         });
     }
 
@@ -41,6 +41,8 @@ class CreateAdministratorsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('administrators');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
