9e17f0d844960820ce2e1d7dd7464c688bc68d66
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-18 20:53:25 -0700

    Updated model features in savelog

--- app/Http/Controllers/version1/LogController.php
+++ app/Http/Controllers/version1/LogController.php
@@ -10,8 +10,8 @@ class LogController extends Controller
     public static function save_log($user_type, $access_token_or_phone_or_email, $log_title, $log_description)
     {
         $log = new Log();
-        $log->user_type = $user_type; 
-        $log->user_id_or_phone_or_email = $access_token_or_phone_or_email;
+        $log->log_user_type = $user_type; 
+        $log->log_user_id_or_phone_or_email = $access_token_or_phone_or_email;
         $log->log_title = $log_title; 
         $log->log_description = $log_description;
         $log->save();
