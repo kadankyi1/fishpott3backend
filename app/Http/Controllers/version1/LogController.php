a0fb9636f8205f3d7b91d8276d8f13329080ce36
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-18 20:50:37 -0700

    Added function save_log to logcontroller

--- app/Http/Controllers/version1/LogController.php
+++ app/Http/Controllers/version1/LogController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class LogController extends Controller
 {
-    //
+    public function save_log($user_type, $access_token_or_phone_or_email, $log_title, $log_description)
+    {
+        $log = new Log();
+        $log->user_type = $user_type; 
+        $log->user_id_or_phone_or_email = $access_token_or_phone_or_email;
+        $log->log_title = $log_title; 
+        $log->log_description = $log_description;
+        $log->save();
+
+    }
 }
