dd39b2e30234196182643e6cb603d84982355c26
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-18 20:15:23 -0700

    Added log model features

--- app/Models/version1/Log.php
+++ app/Models/version1/Log.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'log_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'log_id', 
+        'user_type', 
+        'user_id_or_phone_or_email',
+        'log_title', 
+        'log_description',
+        'created_at',
+        'updated_at',
+    ];
+
 }
