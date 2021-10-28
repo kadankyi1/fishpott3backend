317c96377bf327b2c1e9507742be4f7686045a85
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-28 06:11:36 -0700

    Positive and negative points added to drill model

--- app/Models/version1/DrillAnswer.php
+++ app/Models/version1/DrillAnswer.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class DrillAnswer extends Model
 {
     use HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'drill_answer_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'drill_answer_id', 
+        'drill_answer_sys_id', 
+        'drill_answer_', 
+        'drill_answer_used_for_pott_intelligence_calculation', 
+        'drill_answer_drill_sys_id',
+        'user_investor_id',
+        'created_at',
+        'updated_at',
+    ];
 }
