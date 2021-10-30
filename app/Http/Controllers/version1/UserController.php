89504e9b0b51ab78b7b07160aa51a82945ac088a
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-29 23:13:18 -0700

    Dollar sign added to business revenue

--- app/Http/Controllers/version1/UserController.php
+++ app/Http/Controllers/version1/UserController.php
@@ -837,7 +837,7 @@ public function changePasswordWithResetCode(Request $request)
             $suggestion->business_pitch_video = config('app.url') . '/uploads/pitchvideos/' . $suggestion->business_pitch_video;
             $suggestion->business_full_financial_report_pdf_url = config('app.url') . '/uploads/financedata/' . $suggestion->business_full_financial_report_pdf_url;
             $suggestion->business_net_worth_usd = "$" . UtilController::formatNumberShort($suggestion->business_net_worth_usd);
-            $suggestion->business_lastyr_revenue_usd = UtilController::formatNumberShort($suggestion->business_lastyr_revenue_usd);
+            $suggestion->business_lastyr_revenue_usd = "$" . UtilController::formatNumberShort($suggestion->business_lastyr_revenue_usd);
             $suggestion->business_lastyr_profit_or_loss_usd = UtilController::formatNumberShort($suggestion->business_lastyr_profit_or_loss_usd);
             $suggestion->business_debt_usd = UtilController::formatNumberShort($suggestion->business_debt_usd);
             $suggestion->business_cash_on_hand_usd = UtilController::formatNumberShort($suggestion->business_cash_on_hand_usd);
