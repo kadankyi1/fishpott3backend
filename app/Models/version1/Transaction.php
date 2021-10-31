0d904782b4b19a039594f1b192c243a5b81689ea
kadankyi1 <annodankyikwaku@gmail.com>
2021-10-31 11:07:55 -0700

    Model features for transaction created

--- app/Models/version1/Transaction.php
+++ app/Models/version1/Transaction.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'transaction_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'transaction_id', 
+        'transaction_sys_id',
+        'transaction_type',
+        'transaction_referenced_item_id',
+        'transaction_user_investor_id',
+        'created_at',
+        'updated_at',
+    ];
 }
