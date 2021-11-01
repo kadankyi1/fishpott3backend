7b2ba8f6571d4cc3fbdc473a827a92778b0b82b0
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-01 07:18:25 -0700

    StockValue model and migration created

--- app/Http/Controllers/version1/UserController.php
+++ app/Http/Controllers/version1/UserController.php
@@ -17,6 +17,7 @@ use App\Models\version1\Business;
 use App\Models\version1\Currency;
 use App\Models\version1\Drill;
 use App\Models\version1\DrillAnswer;
+use App\Models\version1\StockOwnership;
 use App\Models\version1\StockPurchase;
 use App\Models\version1\Suggestion;
 use App\Models\version1\SuggestionTypes;
@@ -1574,73 +1575,36 @@ public function changePasswordWithResetCode(Request $request)
         |**************************************************************************
         */
         $data = array();
-        $result = Transaction::where("transaction_user_investor_id", $user->investor_id)->get();
+        $result = StockOwnership::where("stockownership_sys_id", $user->investor_id)->get();
 
-        foreach($result as $transaction){
-            // WITHDRAWAL
-            if($transaction->transaction_transaction_type_id == 1){
-                $withdrawal = Withdrawal::where('withdrawal_sys_id', $transaction->transaction_referenced_item_id)->first();
-                if($withdrawal != null){
-                    if($withdrawal->withdrawal_paid == 0){
-                        $the_status = "Pending";
-                    } else if($withdrawal->withdrawal_paid == 1){
-                        $the_status = "Paid";
-                    } else if($withdrawal->withdrawal_paid == 2){
-                        $the_status = "Cancelled";
-                    } else {
-                        $the_status = "Error";
-                    }
-                    $this_item = array(
-                        'type' => "WITHDRAWAL",
-                        'info_1' => $the_status,
-                        'info_2' => $withdrawal->withdrawal_local_currency_sign . $withdrawal->withdrawal_amt_local,
-                        'info_3' => $withdrawal->withdrawal_receiving_bank_or_momo_name,
-                        'info_4' => $withdrawal->withdrawal_receiving_bank_or_momo_account_number,
-                        'info_5' => date("n M y", strtotime($withdrawal->created_at)),
-                        'info_6' => $withdrawal->withdrawal_sys_id,
-                        'info_6' => $withdrawal->withdrawal_sys_id
-                    );
-                    array_push($data, $this_item);
-                }
-            } 
-            // CREDIT
-            if($transaction->transaction_transaction_type_id == 2){
-
-            }
-            // DIVIDEND
-            if($transaction->transaction_transaction_type_id == 3){
-
-            }
+        foreach($result as $stockownership){
             // STOCK PURCHASE
-            if($transaction->transaction_transaction_type_id == 4){
-                $stockpurchase = StockPurchase::where('stockpurchase_sys_id', $transaction->transaction_referenced_item_id)->first();
-                if($stockpurchase != null){
-                    if($stockpurchase->stockpurchase_processed == 0){
-                        $the_status = "Pending";
-                    } else if($stockpurchase->stockpurchase_processed == 1){
-                        $the_status = "Paid";
-                    } else if($stockpurchase->stockpurchase_processed == 2){
-                        $the_status = "Cancelled";
-                    } else {
-                        $the_status = "Error";
-                    }
-
-                    $currency = Currency::where('currency_id', $stockpurchase->stockpurchase_currency_paid_in_id)->first();
-                    $business = Business::where('business_sys_id', $stockpurchase->stockpurchase_business_id)->first();
-
-                    $this_item = array(
-                        'type' => "SHARES PURCHASE",
-                        'info_1' => $the_status,
-                        'info_2' => $currency->currency_symbol . $stockpurchase->stockpurchase_total_all_fees_in_currency_paid_in,
-                        'info_3' => $business->business_full_name,
-                        'info_4' => strval($stockpurchase->stockpurchase_stocks_quantity),
-                        'info_5' => date("n M y", strtotime($stockpurchase->created_at)),
-                        'info_6' => $stockpurchase->stockpurchase_sys_id
-                    );
-                    array_push($data, $this_item);
-                }
-
+            $business = Business::where('business_sys_id', $stockownership->stockownership_business_id)->first();
+            if($business == null){
+                continue;
             }
+            
+            if($stockpurchase->stockpurchase_processed == 0){
+                $the_status = "Pending";
+            } else if($stockpurchase->stockpurchase_processed == 1){
+                $the_status = "Paid";
+            } else if($stockpurchase->stockpurchase_processed == 2){
+                $the_status = "Cancelled";
+            } else {
+                $the_status = "Error";
+            }
+
+            // GETTING THE COST-PER-SHARE
+            $this_cost_per_share_usd = $stockownership->stockownership_total_cost_usd/$stockownership->stockownership_stocks_quantity;
+
+            $this_item = array(
+                'business_name' => $business->business_full_name,
+                'cost_per_share_usd' => "$" . $this_cost_per_share_usd,
+                'value_per_share_usd' => $business->business_full_name,
+                'quntity_of_stocks' => strval($stockpurchase->stockpurchase_stocks_quantity),
+                'ai_info' => date("n M y", strtotime($stockpurchase->created_at))
+            );
+            array_push($data, $this_item);
         }
         
         return response([
