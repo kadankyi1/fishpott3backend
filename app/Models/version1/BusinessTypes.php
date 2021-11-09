f1b181fe2d5a4ceecafffc86e1b42d5f7d5c9017
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-08 21:36:05 -0800

    Set model attributes for businesstypes

--- app/Models/version1/BusinessTypes.php
+++ app/Models/version1/BusinessTypes.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class BusinessTypes extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'businesstype_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'businesstype_id', 
+        'businesstype_name',
+        'created_at',
+        'updated_at',
+    ];
+
 }
