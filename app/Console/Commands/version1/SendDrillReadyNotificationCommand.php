0c9562bd4e04e2dfbe87a3aca7018c6a2dd368bc
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-09 23:49:56 -0800

    Updating handle function for command of SendDrillReadyNotificationCommand

--- app/Console/Commands/version1/SendDrillReadyNotificationCommand.php
+++ app/Console/Commands/version1/SendDrillReadyNotificationCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\version1\ResetCodeMail;
-use App\Http\Controllers\version1\UserController;
+use App\Http\Controllers\version1\UtilController;
 
 class SendDrillReadyNotificationCommand extends Command
 {
@@ -41,11 +41,34 @@ class SendDrillReadyNotificationCommand extends Command
      */
     public function handle()
     {
-        Log::info("Cron is working fine!");
+        //Log::info("Cron is working fine!");
         // CREATING THE USER OBJECT AND CALLING THE SEND FCM METHOD
         //$user_controller = new UserController();
         //$user_controller->sendFirebaseNotification("New Herald Of Glory", "Added Successfully", "/topics/ALPHA", "ALPHA");
-        Log::info('drill:sendreadynotification Command Run successfully!');
+        //Log::info('drill:sendreadynotification Command Run successfully!');
+
+        $suggestion = UtilController::getLatestSuggestion();
+
+        // CHECKING IF A SUGGESTION EXISTS
+        if($suggestion !=  null && $suggestion != false ){
+            // CHECKING IF A SUGGESTION IS AVAILABLE TO BE NOTIFIED TO USERS
+            if($suggestion->suggestion_notification_sent == false && $suggestion->suggestion_suggestion_type_id == UtilController::getSuggestionType("suggestion_type_name", "Drill", 1)){
+                //
+            } else {
+                //
+            }
+        } else {
+            // NOTIFYING FISHPOTT ADMIN THAT NO DRILLS EXIST
+        }
+
+        // CHECKING SUGGESTION TYPE TO GET IT'S INFO
+        //echo "getSuggestionType: " . UtilController::getSuggestionType("suggestion_type_name", "Business", 1);
+        //echo "suggestion->suggestion_item_reference_id: " . $suggestion->suggestion_item_reference_id; exit;
+        if($suggestion->suggestion_suggestion_type_id == UtilController::getSuggestionType("suggestion_type_name", "Drill", 1)){
+            $suggestion = Drill::where('drill_sys_id', $suggestion->suggestion_item_reference_id)->first();
+            $message = "drill";
+            $country_real_name = "";
+        }
 
         $email_data = array(
             'reset_code' => date('Y-m-d H:i:s'),
