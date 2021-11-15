5ee133406ed85ab76e1df84dbc0c033cf6bcf3be
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-14 23:31:06 -0800

    Added attributes for stock persona

--- app/Models/ai/AiStockPersona.php
+++ app/Models/ai/AiStockPersona.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class AiStockPersona extends Model
 {
     use HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'aistockpersona_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
+    protected $fillable = [
+        'aistockpersona_id', 
+        'aistockpersona_openness_to_experience',
+        'aistockpersona_conscientiousness',
+        'aistockpersona_extraversion',
+        'aistockpersona_agreeableness',
+        'aistockpersona_neuroticism',
+        'created_at',
+        'updated_at',
+    ];
+
 }
