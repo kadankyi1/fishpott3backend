b4bf7c37fc3691798f3551c6f9460a27930955c2
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-14 23:25:07 -0800

    Created stock persona model

---
+++ app/Models/ai/AiStockPersona.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\ai;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AiStockPersona extends Model
+{
+    use HasFactory;
+}
