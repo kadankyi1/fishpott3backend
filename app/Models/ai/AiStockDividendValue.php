692f6726369b7500f83f57aa01c4d62030923a8e
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-14 23:20:46 -0800

    Model attributes set for ai stock dividend model

--- app/Models/ai/AiStockDividendValue.php
+++ app/Models/ai/AiStockDividendValue.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class AiStockDividendValue extends Model
 {
     use HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'aistockdividendvalue_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'aistockdividendvalue_id', 
+        'aistockdividendvalue_value_usd',
+        'aistockdividendvalue_value_datetime',
+        'created_at',
+        'updated_at',
+    ];
+
 }
