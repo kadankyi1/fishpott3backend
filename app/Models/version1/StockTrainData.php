0be3712238bf6ded8f510474f844eba51620d92f
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-17 02:57:37 -0800

    Added features to StockTrainData

--- app/Models/version1/StockTrainData.php
+++ app/Models/version1/StockTrainData.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class StockTrainData extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'stocktraindata_id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'stocktraindata_id', 
+        'stocktraindata_value_per_stock_usd',
+        'stocktraindata_value_change',
+        'stocktraindata_value_volume',
+        'stocktraindata_expected_output',
+        'stocktraindata_admin_adder_id',
+        'created_at',
+        'updated_at',
+    ];
+
 }
