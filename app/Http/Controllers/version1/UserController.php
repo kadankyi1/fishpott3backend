6aa6386401829d30e032d9eeb09f98cdb3263732
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-22 05:40:19 -0800

    Resolving get suggestion error

--- app/Http/Controllers/version1/UserController.php
+++ app/Http/Controllers/version1/UserController.php
@@ -889,7 +889,7 @@ public function changePasswordWithResetCode(Request $request)
         // CHECKING SUGGESTION TYPE TO GET IT'S INFO
         echo "\n\n<br><br>getSuggestionType: " . UtilController::getSuggestionType("suggestion_type_name", "Business", 1);
         echo "\n\n<br><br>suggestion->suggestion_suggestion_type_id: " . $suggestion->suggestion_suggestion_type_id;
-        echo "\n\n<br><br>suggestion->suggestion_item_reference_id: " . $suggestion->suggestion_item_reference_id; exit;
+        echo "\n\n<br><br>suggestion->suggestion_item_reference_id: " . $suggestion->suggestion_item_reference_id;
         if($suggestion->suggestion_suggestion_type_id == UtilController::getSuggestionType("suggestion_type_name", "Drill", 1)){
             echo "\n\n<br><br>here 2";
             $suggestion = Drill::where('drill_sys_id', $suggestion->suggestion_item_reference_id)->first();
