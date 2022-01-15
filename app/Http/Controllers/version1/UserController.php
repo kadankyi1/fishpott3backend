499392bb8a4e3380fa829900e68bf5ef9151716d
kadankyi1 <annodankyikwaku@gmail.com>
2022-01-14 23:57:59 -0800

    Adding pott intelligence calc to drill answer

--- app/Http/Controllers/version1/UserController.php
+++ app/Http/Controllers/version1/UserController.php
@@ -1020,10 +1020,14 @@ public function changePasswordWithResetCode(Request $request)
         // CALCULATING POTT-INTELLIGENCE/OR HOW WELL YOUR POTT KNOWS YOU
         $six_months_ago = date('Y-m-d',strtotime("-180 days")); 
         $answers_six_months_ago = DrillAnswer::where('drill_answer_user_investor_id', $user->investor_id)->whereDate('created_at', ">=" , $six_months_ago)->count(); 
-
+        $pott_user_sync = (intval($answers_six_months_ago)/180) * 100;
+        $user->user_pott_intelligence = $pott_user_sync;
+        $user->save();
+        /*
         echo "\n six_months_ago: " .  $six_months_ago;
         echo "\n answers_six_months_ago: " .  $answers_six_months_ago;
         exit;
+        */
         
 
         // GETTING THE ANSWERS OF FRIENDS
