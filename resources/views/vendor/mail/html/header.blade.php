afa29a54e93eae27516c22f5e5b12c482e945763
kadankyi1 <annodankyikwaku@gmail.com>
2022-01-23 03:14:50 -0800

    Added email sending for signups

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img style="height: 75px; width: 75px; margin-top: -20px;margin-left: -5px;" src="http://fishpott.local/images/fishpott_icon_circle.png" class="logo" alt="FishPott Logo">
+<img style="height: 75px; width: 75px; margin-top: -20px;margin-left: -5px;" src="https://app.fishpott.com/images/fishpott_icon_circle.png" class="logo" alt="FishPott Logo">
 <p style="font-size: 35px; margin-top: 2px; margin-bottom: 0px">{{ $slot }}</p>
 </a>
 </td>
