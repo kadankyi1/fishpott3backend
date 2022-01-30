f313e7b36ae7ed54185f6081ad371b56f6e587cc
kadankyi1 <annodankyikwaku@gmail.com>
2022-01-30 01:32:14 -0800

    Added check for sellback transaction viewing

--- app/Http/Controllers/version1/UserController.php
+++ app/Http/Controllers/version1/UserController.php
@@ -2043,7 +2043,7 @@ public function changePasswordWithResetCode(Request $request)
                 }
 
             }
-
+        }
         return response([
             "status" => 1, 
             "message" => "success",
