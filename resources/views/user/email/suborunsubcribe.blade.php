0785e77a062235256a21cd8235328883ad146692
kadankyi1 <annodankyikwaku@gmail.com>
2022-02-16 22:44:38 -0800

    Creating unsubcribe page

---
+++ resources/views/user/email/suborunsubcribe.blade.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login | Admin</title>
+    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 10]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <!-- Meta -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Favicon icon -->
+
+      <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+      <!-- Google font-->     
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+      <!-- Required Fremwork -->
+      <link rel="stylesheet" type="text/css" href="/css/bootstrap/css/bootstrap.min.css">
+      <!-- waves.css -->
+      <link rel="stylesheet" href="/pages/waves/css/waves.min.css" type="text/css" media="all">
+      <!-- themify-icons line icon -->
+      <link rel="stylesheet" type="text/css" href="/icon/themify-icons/themify-icons.css">
+      <!-- ico font -->
+      <link rel="stylesheet" type="text/css" href="/icon/icofont/css/icofont.css">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" type="text/css" href="/icon/font-awesome/css/font-awesome.min.css">
+      <!-- Style.css -->
+      <link rel="stylesheet" type="text/css" href="/css/style.css">
+  </head>
+
+  <body themebg-pattern="theme1">
+  <!-- Pre-loader start -->
+  <div class="theme-loader">
+      <div class="loader-track">
+          <div class="preloader-wrapper">
+              <div class="spinner-layer spinner-blue">
+                  <div class="circle-clipper left">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="gap-patch">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="circle-clipper right">
+                      <div class="circle"></div>
+                  </div>
+              </div>
+              <div class="spinner-layer spinner-red">
+                  <div class="circle-clipper left">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="gap-patch">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="circle-clipper right">
+                      <div class="circle"></div>
+                  </div>
+              </div>
+            
+              <div class="spinner-layer spinner-yellow">
+                  <div class="circle-clipper left">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="gap-patch">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="circle-clipper right">
+                      <div class="circle"></div>
+                  </div>
+              </div>
+            
+              <div class="spinner-layer spinner-green">
+                  <div class="circle-clipper left">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="gap-patch">
+                      <div class="circle"></div>
+                  </div>
+                  <div class="circle-clipper right">
+                      <div class="circle"></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Pre-loader end -->
+
+    <section class="login-block">
+        <!-- Container-fluid starts -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <!-- Authentication card start -->
+
+                        <div class="md-float-material form-material">
+                            <div class="text-center">
+                                <img src="/images/fishpott_icon_circle.png" style="height: 100px; width: 100px;" alt="logo.png">
+                            </div>
+                            <div class="auth-box card">
+                                <div class="card-block">
+                                    <div class="row m-b-20">
+                                        <div class="col-md-12">
+                                            <h3 class="text-center">Sign In</h3>
+
+                                            <div class="loader-holder offset-md-5" id="loader"><br><br><br><br><br><br><div class="myloader"></div></div>
+                                        </div>
+                                    </div>
+                                    <form id="lform">
+                                        <div class="form-group form-primary">
+                                            <input type="text" id="administrator_phone_number" name="administrator_phone_number" class="form-control" required="">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Phone</label>
+                                        </div>
+                                        <div class="form-group form-primary">
+                                            <input type="password" id="password" name="password" class="form-control" required="">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Password</label>
+                                        </div>
+                                        <div class="form-group form-primary">
+                                            <input type="text" id="administrator_user_pottname" name="administrator_user_pottname" class="form-control" required="">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Pott Name</label>
+                                        </div>
+                                        <div class="form-group form-primary">
+                                            <input type="password" id="administrator_pott_password" name="administrator_pott_password" class="form-control" required="">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Pott Password</label>
+                                        </div>
+                                        <div class="form-group form-primary">
+                                            <input type="password" id="frontend_key" name="frontend_key" class="form-control" required="">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Client Key</label>
+                                        </div>
+                                        <div class="row m-t-30">
+                                            <div class="col-md-12">
+                                                <input type="submit" value="Sign In" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20"/>
+                                            </div>
+                                        </div>
+                                        <hr/>
+                                        <div class="row">
+                                            <div class="col-md-10">
+                                                <p class="text-inverse text-left m-b-0">Thank you.</p>
+                                                <p class="text-inverse text-left"><a href="https://play.google.com/store/apps/details?id=com.fishpott.fishpott5&hl=en&gl=US"><b>Go To The App</b></a></p>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img src="/images/auth/Logo-small-bottom.png" alt="small-logo.png">
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end of form -->
+                </div>
+                <!-- end of col-sm-12 -->
+            </div>
+            <!-- end of row -->
+        </div>
+        <!-- end of container-fluid -->
+    </section>
+    <!-- Warning Section Starts -->
+    <!-- Older IE warning message -->
+    <!--[if lt IE 10]>
+<div class="ie-warning">
+    <h1>Warning!!</h1>
+    <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
+    <div class="iew-container">
+        <ul class="iew-download">
+            <li>
+                <a href="http://www.google.com/chrome/">
+                    <img src="images/browser/chrome.png" alt="Chrome">
+                    <div>Chrome</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mozilla.org/en-US/firefox/new/">
+                    <img src="images/browser/firefox.png" alt="Firefox">
+                    <div>Firefox</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://www.opera.com">
+                    <img src="images/browser/opera.png" alt="Opera">
+                    <div>Opera</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.apple.com/safari/">
+                    <img src="images/browser/safari.png" alt="Safari">
+                    <div>Safari</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                    <img src="images/browser/ie.png" alt="">
+                    <div>IE (9 & above)</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <p>Sorry for the inconvenience!</p>
+</div>
+<![endif]-->
+<!-- Warning Section Ends -->
+<!-- Required Jquery -->
+    <script type="text/javascript" src="/js/jquery/jquery.min.js"></script>     
+    <script type="text/javascript" src="/js/jquery-ui/jquery-ui.min.js "></script>     
+    <script type="text/javascript" src="/js/popper.js/popper.min.js"></script>     
+    <script type="text/javascript" src="/js/bootstrap/js/bootstrap.min.js "></script>
+<!-- waves js -->
+<script src="/pages/waves/js/waves.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="/js/jquery-slimscroll/jquery.slimscroll.js "></script>
+<!-- modernizr js -->
+    <script type="text/javascript" src="/js/SmoothScroll.js"></script>     
+    <script src="/js/jquery.mCustomScrollbar.concat.min.js "></script>
+<!-- i18next.min.js -->
+<script type="text/javascript" src="/bower_components/i18next/js/i18next.min.js"></script>
+<script type="text/javascript" src="/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
+<script type="text/javascript" src="/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
+<script type="text/javascript" src="/bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
+<script type="text/javascript" src="/js/common-pages.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-noty/2.3.7/packaged/jquery.noty.packaged.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
+
+<script type="text/javascript" src="/js/custom/config.js"></script>
+<script type="text/javascript" src="/js/custom/login.js"></script>
+</body>
+
+</html>
